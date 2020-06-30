f1c0ec4ae5e842214097ea5ffeef06bdd8817f06
Thao Nguyen <vanthao10a8@gmail.com>
2020-06-30 16:19:53 +0700

    Init source project

---
+++ confirm-dialog-research/src/components/ConfirmDelete.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-modal v-model="showModal" :title="title" @ok="handleOk">
+    <p class="my-4">{{ msg }}</p>
+  </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title : {
+      type : String
+    },
+    msg : {
+      type : String
+    },
+    handleOk : {
+      type : Function
+    },
+    modalShow : {
+      type : Boolean
+    }
+  },
+  computed : {
+    showModal : {
+      get : function(){
+        return this.modalShow
+      },
+      set : function(value){
+        return this.$emit('update:modalShow', value)
+      }
+    }
+  },
+};
+</script>
---
+++ confirm-dialog-research/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ConfirmDeleteModal
+      :modalShow.sync="modalShow"
+      :title="title"
+      :msg="deleteMsg"
+      :handleOk="handleSubmit"
+    ></ConfirmDeleteModal>
+    <!-- person details -->
+    <b-button @click="confirmDelete(true)">Launch demo modal</b-button>
+  </div>
+</template>
+
+<script>
+import ConfirmDeleteModal from "@/components/ConfirmDelete";
+export default {
+  components: {
+    ConfirmDeleteModal,
+  },
+  data() {
+    return {
+      modalShow: false,
+      deleteMsg: "",
+      title: "",
+    };
+  },
+  methods: {
+    confirmDelete(val) {
+      this.modalShow = val;
+      this.title = "Delete Code?";
+      this.deleteMsg = "You are deleting a code";
+    },
+    handleSubmit() {
+      this.modalShow = false;
+      alert("Submit oke");
+    },
+  },
+};
+</script>
